4b2c748a1a39185a0b9c23a7da44548bfccabe9b
xuanbo <wxb740119>
2017-02-28 10:01:05 +0800

    spring cloud config进行配置管理

--- web/src/main/java/com/whut/stsm/web/controller/HomeController.java
+++ web/src/main/java/com/whut/stsm/web/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.whut.stsm.web.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +13,15 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class HomeController {
 
+    /**
+     * cloud-config-repo中的属性
+     */
+    @Value("${hello}")
+    private String hello;
+
     @GetMapping({"/", "/index"})
     public String index(ModelMap modelMap, HttpSession session) {
-        modelMap.addAttribute("msg", "welcome!");
+        modelMap.addAttribute("msg", hello);
         modelMap.addAttribute("sessionId", session.getId());
         return "index";
     }
