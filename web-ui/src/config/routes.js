673b9d334f1f1dde7419b5eb5c4049d8fd290979
xuanbo <1345545983@qq.com>
2017-03-16 07:31:49 +0800

    修改

--- web-ui/src/config/routes.js
+++ web-ui/src/config/routes.js
@@ -1,18 +1,18 @@
 // 引用模板
-import index from '../components/index'
-import notFound from '../components/notFound'
+import index from 'components/index'
+import notFound from 'components/notFound'
 
 // team
-import teamIndex from '../components/team/index'
-import teamUser from '../components/team/user'
+import teamIndex from 'components/team/index'
+import teamUser from 'components/team/user'
 
 // process
-import processIndex from '../components/process/index'
-import processStart from '../components/process/start'
+import processIndex from 'components/process/index'
+import processStart from 'components/process/start'
 
 // task
-import taskShow from '../components/task/show'
-import taskConfirm from '../components/task/confirm'
+import taskShow from 'components/task/show'
+import taskConfirm from 'components/task/confirm'
 
 // 配置路由
 export default [{
